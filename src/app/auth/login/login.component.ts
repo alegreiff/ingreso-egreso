3d670cc3690c5709d24d656f185351f51bc372f5
Jaime de Greiff <alegreiff@gmail.com>
2020-06-26 21:26:32 -0500

    Quitado HTML

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
 
-  constructor() { }
+  ngOnInit(){
+    this.loginForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+  autenticaUsuario(){
+    if(this.loginForm.invalid){ return; }
+    console.log(this.loginForm.value)
+    const { correo, password } = this.loginForm.value;
+    Swal.fire({
+      title: 'Espere por favor',
 
-  ngOnInit(): void {
+      onBeforeOpen: () => {
+        Swal.showLoading()
+
+      }
+    });
+
+    this.authService.authUsuario(correo, password)
+    .then( credenciales  => {
+      console.log( credenciales )
+      Swal.close();
+      this.router.navigate(['/'])
+    })
+    .catch(err => {
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: err.message
+      })
+    })
+
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAlEQfNtlmvyWTA9IiX8Op6IM8Cdf8KGAU",
+    authDomain: "ingreso-egreso-app-biko.firebaseapp.com",
+    databaseURL: "https://ingreso-egreso-app-biko.firebaseio.com",
+    projectId: "ingreso-egreso-app-biko",
+    storageBucket: "ingreso-egreso-app-biko.appspot.com",
+    messagingSenderId: "123800639739",
+    appId: "1:123800639739:web:66b6baeb2d324e4de6f85e"
+  }
 };
 
 /*
